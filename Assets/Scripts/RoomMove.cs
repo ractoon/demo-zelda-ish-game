6e85b2ad9e987a8539af79244b21fcd6f66f954d
ractoon <ractoon@gmail.com>
2021-01-11 21:57:05 -0700

    Room transitions

---
+++ Assets/Scripts/RoomMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomMove : MonoBehaviour
+{
+    public Vector2 cameraChangeMin;
+    public Vector2 cameraChangeMax;
+    public Vector3 playerChange;
+    private CameraMovement cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.GetComponent<CameraMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.tag == "Player") {
+            cam.minPosition.x = cameraChangeMin.x;
+            cam.maxPosition.x = cameraChangeMax.x;
+            cam.minPosition.y = cameraChangeMin.y;
+            cam.maxPosition.y = cameraChangeMax.y;
+            other.transform.position += playerChange;
+        }
+    }
+}
