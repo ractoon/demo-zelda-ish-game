30cdda3d2d199f2401b2737f952f6a753e90d40e
ractoon <ractoon@gmail.com>
2021-01-12 21:50:39 -0700

    Room labels

--- Assets/Scripts/RoomMove.cs
+++ Assets/Scripts/RoomMove.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomMove : MonoBehaviour
 {
@@ -8,6 +9,10 @@ public class RoomMove : MonoBehaviour
     public Vector2 cameraChangeMax;
     public Vector3 playerChange;
     private CameraMovement cam;
+    public bool needText;
+    public string placeName;
+    public GameObject text;
+    public Text placeText;
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +34,18 @@ public class RoomMove : MonoBehaviour
             cam.minPosition.y = cameraChangeMin.y;
             cam.maxPosition.y = cameraChangeMax.y;
             other.transform.position += playerChange;
+
+            if (needText) {
+                StartCoroutine(placeNameCo());
+            }
         }
+    }
+
+    private IEnumerator placeNameCo()
+    {
+        text.SetActive(true);
+        placeText.text = placeName;
+        yield return new WaitForSeconds(4f);
+        text.SetActive(false);
     }
 }
