57c66856eba29b70c7b6985bede328aff79ba32a
ractoon <ractoon@gmail.com>
2021-01-23 15:43:49 -0700

    Enemy animations

--- Assets/Scripts/Log.cs
+++ Assets/Scripts/Log.cs
@@ -9,12 +9,14 @@ public class Log : Enemy
     public float chaseRadius;
     public float attackRadius;
     public Transform homePosition;
+    public Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         currentState = EnemyState.idle;
         myRidigbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         target = GameObject.FindWithTag("Player").transform;
     }
 
@@ -30,8 +32,35 @@ public class Log : Enemy
         && Vector3.Distance(target.position, transform.position) > attackRadius) {
             if ((currentState == EnemyState.idle || currentState == EnemyState.walk) && currentState != EnemyState.stagger) {
                 Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+                ChangeAnim(temp - transform.position);
                 myRidigbody.MovePosition(temp);
                 ChangeState(EnemyState.walk);
+                anim.SetBool("wakeUp", true);
+            }
+        } else if (Vector3.Distance(target.position, transform.position) > chaseRadius ) {
+            anim.SetBool("wakeUp", false);
+        }
+    }
+
+    private void SetAnimFloat(Vector2 setVector) 
+    {
+        anim.SetFloat("moveX", setVector.x);
+        anim.SetFloat("moveY", setVector.y);
+    }
+    
+    private void ChangeAnim(Vector2 direction)
+    {
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y)) {
+            if (direction.x > 0) {
+                SetAnimFloat(Vector2.right);
+            } else if (direction.x < 0) {
+                SetAnimFloat(Vector2.left);
+            }
+        } else if (Mathf.Abs(direction.x) < Mathf.Abs(direction.y)) {
+            if (direction.y > 0) {
+                SetAnimFloat(Vector2.up);
+            } else if (direction.y < 0) {
+                SetAnimFloat(Vector2.down);
             }
         }
     }
