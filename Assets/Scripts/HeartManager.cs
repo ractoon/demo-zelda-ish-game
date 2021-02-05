21c5e420fc3f93a8396fceb41cd4ae361c2ef6e3
ractoon <ractoon@gmail.com>
2021-02-05 14:51:35 -0700

    Player health

--- Assets/Scripts/HeartManager.cs
+++ Assets/Scripts/HeartManager.cs
@@ -10,6 +10,7 @@ public class HeartManager : MonoBehaviour
     public Sprite halfFullHeart;
     public Sprite emptyHeart;
     public FloatValue heartContainers;
+    public FloatValue playerCurrentHealth;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +23,21 @@ public class HeartManager : MonoBehaviour
         for (int i = 0; i < heartContainers.initialValue; i++) {
             hearts[i].gameObject.SetActive(true);
             hearts[i].sprite = fullHeart;
+        }
+    }
+
+    public void UpdateHearts()
+    {
+        float tempHealth = playerCurrentHealth.RuntimeValue / 2;
+
+        for (int i = 0; i < heartContainers.initialValue; i++) {
+            if (i <= tempHealth - 1) {      // full heart
+                hearts[i].sprite = fullHeart;
+            } else if (i >= tempHealth) {        // empty heart
+                hearts[i].sprite = emptyHeart;
+            } else {    // half full heart
+                hearts[i].sprite = halfFullHeart;
+            }
         }
     }
 }
--- Assets/Scripts/Scriptable Objects/FloatValue.cs
+++ Assets/Scripts/Scriptable Objects/FloatValue.cs
@@ -3,7 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu]
-public class FloatValue : ScriptableObject
+public class FloatValue : ScriptableObject, ISerializationCallbackReceiver
 {
     public float initialValue;
+
+    [HideInInspector]
+    public float RuntimeValue;
+
+    public void OnBeforeSerialize()
+    {
+        RuntimeValue = initialValue;
+    }
+
+    public void OnAfterDeserialize()
+    {
+
+    }
 }
